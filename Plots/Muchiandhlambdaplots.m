1b727786df8ced2d9a42e51262bf67417d62600f
David Carcamo <39925934+dpcarcamo@users.noreply.github.com>
2026-07-05 16:59:46 -0400

    Update Figure 1 plotting style

--- Plots/Muchiandhlambdaplots.m
+++ Plots/Muchiandhlambdaplots.m
@@ -15,13 +15,24 @@ addpath(fullfile(repoRoot, 'Hippo'))
 addpath(fullfile(repoRoot, 'Stringer'))
 
 files = ["Allenhldata.mat", "hippomuchidata.mat", "stringerhldata.mat"];
-markers = {'square', 'o', '^'};
-colors = ["#1f4fd6", "#c65100", "#167a43"];
+dataMarker = 'o';
+colors = ["#2676ad", "#4f4cc4", "#2f682c"];
+referenceLineColor = "#939598";
 markerSize = 70;
+markerEdgeColor = [0 0 0];
+markerLineWidth = 0.5;
+markerFaceAlpha = 0.75;
+fullMarkerSizeRange = [45 220];
+fullNRange = fullDataNRange(files);
+axisLabelFontSize = 30;
+tickLabelFontSize = 18;
+hZeroPlot = -0.00005;
+criticalPointSize = 45;
+modelYLim = [0, 2.5];
 
 % -1 denotes the final/full data point.
-%figureSets = {[-1]};
-figureSets = {[20 100 1000 -1]};
+figureSets = {[-1]};
+%figureSets = {[20 100 1000 -1]};
 
 %% Plot mu and chi from data
 
@@ -30,6 +41,7 @@ for f = 1:length(figureSets)
     hold on
 
     currentSet = figureSets{f};
+    scaleFullDataMarkers = isFullDataOnlySet(currentSet);
 
     for i = 1:length(files)
         [Data, ~, ~] = loadDataFile(files(i));
@@ -41,32 +53,54 @@ for f = 1:length(figureSets)
 
         for val = currentSet
             if val == -1
-                scatter(lastofarray(mus), lastofarray(chis), markerSize, ...
-                    'Marker', markers{i}, ...
-                    'MarkerEdgeColor', cFull, ...
-                    'LineWidth', 1.5)
+                if scaleFullDataMarkers
+                    fullNs = lastofarray(Nss);
+                    fullMarkerSizes = markerSizeFromN(fullNs, fullNRange, fullMarkerSizeRange);
+                    scatter(lastofarray(mus), lastofarray(chis), fullMarkerSizes, ...
+                        'Marker', dataMarker, ...
+                        'MarkerEdgeColor', markerEdgeColor, ...
+                        'MarkerFaceColor', cFull, ...
+                        'MarkerFaceAlpha', markerFaceAlpha, ...
+                        'LineWidth', markerLineWidth)
+                else
+                    scatter(lastofarray(mus), lastofarray(chis), markerSize, ...
+                        'Marker', dataMarker, ...
+                        'MarkerEdgeColor', markerEdgeColor, ...
+                        'MarkerFaceColor', cFull, ...
+                        'MarkerFaceAlpha', markerFaceAlpha, ...
+                        'LineWidth', markerLineWidth)
+                end
             elseif val == 0
                 continue
             else
                 [~, I] = min(abs(Nss - val), [], dim, "linear");
+                pointColor = colorForN(val, c20, c100, c1000, cFull);
                 scatter(mus(I), chis(I), markerSize, ...
-                    'Marker', markers{i}, ...
-                    'MarkerEdgeColor', colorForN(val, c20, c100, c1000, cFull), ...
-                    'LineWidth', 1.5)
+                    'Marker', dataMarker, ...
+                    'MarkerEdgeColor', markerEdgeColor, ...
+                    'MarkerFaceColor', pointColor, ...
+                    'MarkerFaceAlpha', markerFaceAlpha, ...
+                    'LineWidth', markerLineWidth)
             end
         end
     end
 
     x = linspace(-1, 0, 1000);
-    plot(x, 1 - x.^2, 'LineWidth', 2, 'Color', 'blue')
+    plot(x, 1 - x.^2, 'LineWidth', 2, 'Color', referenceLineColor)
 
     ylim([0.05, 70])
     xlim([-1, -0.8])
     axis square
-    xlabel('\mu', 'FontSize', 18)
-    ylabel('\chi', 'FontSize', 18)
+    xlabel('Average activity $\langle \mu \rangle$', ...
+        'Interpreter', 'latex', ...
+        'FontSize', axisLabelFontSize)
+    ylabel('Susceptibility $\chi$', ...
+        'Interpreter', 'latex', ...
+        'FontSize', axisLabelFontSize)
     set(gca, 'YScale', 'log')
     set(gca, 'TickDir', 'both')
+    ax = gca;
+    ax.FontSize = tickLabelFontSize;
     box on
 end
 
@@ -76,10 +110,11 @@ for f = 1:length(figureSets)
     figure
     hold on
 
-    plot(-0.00005 + 0 * linspace(1, 2), linspace(1, 2.5), 'r', 'LineWidth', 2)
-    plot(linspace(-1.4, -0.00001), 0 * linspace(1, 2.5), 'b', 'LineWidth', 2)
+    plot(hZeroPlot + 0 * linspace(1, 2), linspace(1, 2.5), 'r', 'LineWidth', 2)
+    plot(linspace(-1.4, -0.00001), 0 * linspace(1, 2.5), 'Color', referenceLineColor, 'LineWidth', 2)
 
     currentSet = figureSets{f};
+    scaleFullDataMarkers = isFullDataOnlySet(currentSet);
 
     for i = 1:length(files)
         [Data, hs, ls] = loadDataFile(files(i));
@@ -91,31 +126,107 @@ for f = 1:length(figureSets)
 
         for val = currentSet
             if val == -1
-                scatter(lastofarray(hs), lastofarray(ls), markerSize, ...
-                    'Marker', markers{i}, ...
-                    'MarkerEdgeColor', cFull, ...
-                    'LineWidth', 1.5)
+                if scaleFullDataMarkers
+                    fullNs = lastofarray(Nss);
+                    fullMarkerSizes = markerSizeFromN(fullNs, fullNRange, fullMarkerSizeRange);
+                    scatter(lastofarray(hs), lastofarray(ls), fullMarkerSizes, ...
+                        'Marker', dataMarker, ...
+                        'MarkerEdgeColor', markerEdgeColor, ...
+                        'MarkerFaceColor', cFull, ...
+                        'MarkerFaceAlpha', markerFaceAlpha, ...
+                        'LineWidth', markerLineWidth)
+                else
+                    scatter(lastofarray(hs), lastofarray(ls), markerSize, ...
+                        'Marker', dataMarker, ...
+                        'MarkerEdgeColor', markerEdgeColor, ...
+                        'MarkerFaceColor', cFull, ...
+                        'MarkerFaceAlpha', markerFaceAlpha, ...
+                        'LineWidth', markerLineWidth)
+                end
             elseif val == 0
                 continue
             else
                 [~, I] = min(abs(Nss - val), [], dim, "linear");
+                pointColor = colorForN(val, c20, c100, c1000, cFull);
                 scatter(hs(I), ls(I), markerSize, ...
-                    'Marker', markers{i}, ...
-                    'MarkerEdgeColor', colorForN(val, c20, c100, c1000, cFull), ...
-                    'LineWidth', 1.5)
+                    'Marker', dataMarker, ...
+                    'MarkerEdgeColor', markerEdgeColor, ...
+                    'MarkerFaceColor', pointColor, ...
+                    'MarkerFaceAlpha', markerFaceAlpha, ...
+                    'LineWidth', markerLineWidth)
             end
         end
     end
 
+    scatter(hZeroPlot, 1, criticalPointSize, ...
+        'Marker', dataMarker, ...
+        'MarkerFaceColor', 'r', ...
+        'MarkerEdgeColor', 'r')
+
     axis square
-    xlabel('h', 'FontSize', 20)
-    ylabel('\lambda', 'FontSize', 20)
+    xlabel('External field $h$', ...
+        'Interpreter', 'latex', ...
+        'FontSize', axisLabelFontSize)
+    ylabel('Interaction strength $J$', ...
+        'Interpreter', 'latex', ...
+        'FontSize', axisLabelFontSize)
     xscale log
     box on
     set(gca, 'TickDir', 'both')
-    xlim([-1, -0.00005])
-    ylim([0, 2.5])
+    xlim([-1, hZeroPlot])
+    xticks([-1, -0.1, -0.01, -0.001, -0.0001, hZeroPlot])
+    xticklabels({'-10^{0}', '-10^{-1}', '-10^{-2}', '-10^{-3}', ...
+        sprintf('-10^{-4}\n'), sprintf('\n0')})
+    ylim(modelYLim)
+    ax = gca;
+    ax.FontSize = tickLabelFontSize;
+    ax.TickLabelInterpreter = 'tex';
 end
+
+%% Plot full data in h and J with a linear h axis
+
+figure
+hold on
+
+plot([0, 0], [1,modelYLim(2)], 'r', 'LineWidth', 2)
+plot([-1, 1], [0, 0], 'Color', referenceLineColor, 'LineWidth', 2)
+
+for i = 1:length(files)
+    [Data, hs, ls] = loadDataFile(files(i));
+    [Nss, ~, ~] = summarizeData(Data);
+
+    baseColor = hex2rgb(colors(i));
+    [~, ~, ~, cFull] = dataAmountColors(baseColor);
+
+    fullNs = lastofarray(Nss);
+    fullMarkerSizes = markerSizeFromN(fullNs, fullNRange, fullMarkerSizeRange);
+
+    scatter(lastofarray(hs), lastofarray(ls), fullMarkerSizes, ...
+        'Marker', dataMarker, ...
+        'MarkerEdgeColor', markerEdgeColor, ...
+        'MarkerFaceColor', cFull, ...
+        'MarkerFaceAlpha', markerFaceAlpha, ...
+        'LineWidth', markerLineWidth)
+end
+
+scatter(0, 1, criticalPointSize, ...
+    'Marker', dataMarker, ...
+    'MarkerFaceColor', 'r', ...
+    'MarkerEdgeColor', 'r')
+
+axis square
+xlim([-1, 1])
+ylim(modelYLim)
+xlabel('External field $h$', ...
+    'Interpreter', 'latex', ...
+    'FontSize', axisLabelFontSize)
+ylabel('Interaction strength $J$', ...
+    'Interpreter', 'latex', ...
+    'FontSize', axisLabelFontSize)
+box on
+set(gca, 'TickDir', 'both')
+ax = gca;
+ax.FontSize = tickLabelFontSize;
 
 function [Data, hs, ls] = loadDataFile(fileName)
     obj = load(fileName);
@@ -147,6 +258,33 @@ function [c20, c100, c1000, cFull] = dataAmountColors(baseColor)
     c100 = lightenColor(baseColor, 0.45);
     c1000 = lightenColor(baseColor, 0.19);
     cFull = baseColor;
+end
+
+function tf = isFullDataOnlySet(currentSet)
+    tf = numel(currentSet) == 1 && currentSet == -1;
+end
+
+function nRange = fullDataNRange(files)
+    allFullNs = [];
+
+    for i = 1:length(files)
+        [Data, ~, ~] = loadDataFile(files(i));
+        [Nss, ~, ~] = summarizeData(Data);
+        allFullNs = [allFullNs, lastofarray(Nss)];
+    end
+
+    allFullNs = allFullNs(isfinite(allFullNs));
+    nRange = [min(allFullNs), max(allFullNs)];
+end
+
+function markerSizes = markerSizeFromN(Ns, nRange, sizeRange)
+    if nRange(1) == nRange(2)
+        markerSizes = mean(sizeRange) * ones(size(Ns));
+        return
+    end
+
+    scaledN = (Ns - nRange(1)) ./ diff(nRange);
+    markerSizes = sizeRange(1) + scaledN .* diff(sizeRange);
 end
 
 function c = colorForN(N, c20, c100, c1000, cFull)
